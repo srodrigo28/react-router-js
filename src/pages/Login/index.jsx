d4fe20a18909c00c4f8e57028d6d0ab49de55339
Sebastiao <rodrigoexer1@gmail.com>
2023-05-30 06:54:47 -0300

    create-state-form

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,25 +1,45 @@
 import { Container } from "./style";
-
+import { useState } from "react"
+/** Use state treinando...
+ * https://www.youtube.com/watch?v=keEUn64Ceig&t=227s 
+ */
 export function Login(){
+    const [usuario, setusuario] = useState();
+    const [senha, setSenha] = useState();
+
     function handleLogin(e){
         e.preventDefault()
-        
+
         console.log("Estou logando ... aguarde ...")
+        console.log(" Usuário: " + usuario + "\n Senha: " + senha) ;
+
+        if( usuario === "admin" & senha === "123"){
+            console.log("Usuário: Administrador")
+        }else{
+            console.log("Usuário: não encontrado :(")
+        }
     }
     return(
         <Container>
             <h1>Login</h1>
-
-            <form onSubmit={handleLogin}>
-                <div className="group-input">
-                    <label htmlFor="usuario">Usuario</label>
-                    <input type="text" id="usuario" />
+            <form onSubmit={handleLogin} className="form">
+                <div className="input-group">
+                    <label>Usuario</label>
+                    <input 
+                        type="text"
+                        placeholder="usuário cadastrado"
+                        onChange={ (e) => setusuario(e.target.value) }
+                    />
                 </div>
-                <div className="group-input">
-                <label htmlFor="senha">Senha</label>
-                    <input type="password" id="senha" />
+                <div className="input-group">
+                    <label>Senha</label>
+                    <input 
+                        type="password"
+                        placeholder="*****************"
+                        onChange={ (e) => setSenha(e.target.value) }    
+                    />
                 </div>
-                <div className="group-input">
+                <div className="input-group">
                     <button>Entrar</button>
                 </div>
 
--- src/pages/Login/style.jsx
+++ src/pages/Login/style.jsx
@@ -1,3 +1,59 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+        height: 100vh;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        justify-content: center;
+
+        > form{
+            display: flex;
+            align-items: center;
+            flex-direction: column;
+            justify-content: center;
+
+            background: #fff;
+
+            .input-group{
+                padding: 20px;
+            }
+
+            input{
+                padding: 10px;
+                background-color: red;
+                color: #fff;
+                font-weight: 500;
+                font-size: 1.2em;
+                border-radius: 7px;
+                border:none;
+                outline: none;
+            }
+            input::placeholder{
+                color: #fff;
+            }
+            input[type="text"]{
+                width: 100%;
+                height: 52px;
+                box-sizing: border-box;
+            }
+            input[type="password"]{
+                width: 100%;
+                padding-top: 20px;
+                text-align: center;
+            }
+            button{
+                width: 250px;
+                border: none;
+                height: 52px;
+                color: #fff;
+                border-radius: 7px;
+                background: darkviolet;
+                cursor: pointer;
+                transition: .5;
+            }
+            button:hover {
+                opacity: .9;
+            }
+        }
+`;
