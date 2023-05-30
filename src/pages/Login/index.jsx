6c499528413690bf9cfb21b3a2ee491fdd4aec3d
Sebastiao <rodrigoexer1@gmail.com>
2023-05-30 06:00:30 -0300

    update-at-login-form-create

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,9 +1,29 @@
 import { Container } from "./style";
 
 export function Login(){
+    function handleLogin(e){
+        e.preventDefault()
+        
+        console.log("Estou logando ... aguarde ...")
+    }
     return(
         <Container>
             <h1>Login</h1>
+
+            <form onSubmit={handleLogin}>
+                <div className="group-input">
+                    <label htmlFor="usuario">Usuario</label>
+                    <input type="text" id="usuario" />
+                </div>
+                <div className="group-input">
+                <label htmlFor="senha">Senha</label>
+                    <input type="password" id="senha" />
+                </div>
+                <div className="group-input">
+                    <button>Entrar</button>
+                </div>
+
+            </form>
         </Container>
     )
 }
